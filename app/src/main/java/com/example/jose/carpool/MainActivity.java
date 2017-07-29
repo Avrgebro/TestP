96a58bdd6891a1bf3c518c4d5a7adf9059708bd0
Jose Bejarano Carranza <jabcarranz@gmail.com>
2017-07-29 15:51:15 -0500

    Generalizacion de estilos EditText

--- app/src/main/java/com/example/jose/carpool/MainActivity.java
+++ app/src/main/java/com/example/jose/carpool/MainActivity.java
@@ -95,10 +95,6 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
