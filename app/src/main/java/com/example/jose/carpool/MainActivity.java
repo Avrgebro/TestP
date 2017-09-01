731de5e2d47164c03ed3cb9d63ce7bab3e8f678d
Alexahpa <a.huamani@pucp.pe>
2017-09-01 00:01:37 -0500

    Pick photo car
    Falta el json

--- app/src/main/java/com/example/jose/carpool/MainActivity.java
+++ app/src/main/java/com/example/jose/carpool/MainActivity.java
@@ -159,6 +159,8 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
--- app/src/main/java/com/example/jose/carpool/tus_vehiculos.java
+++ app/src/main/java/com/example/jose/carpool/tus_vehiculos.java
@@ -1,19 +1,46 @@
 package com.example.jose.carpool;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.Instrumentation;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.icu.util.Output;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.os.ResultReceiver;
+import android.provider.Contacts;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContentResolverCompat;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.FileProvider;
+import android.support.v4.os.EnvironmentCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.AppCompatButton;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +48,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -32,12 +60,20 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -53,6 +89,18 @@ public class tus_vehiculos extends Fragment {
     private SwipeRefreshLayout swipeView;
     private int auxI;
 
+    private Uri photoURI;
+    private static final int REQUEST_CAMERA = 1;
+    private static final int PICK_FROM_FILE =2;
+
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1234;
+    private static final int MY_PERMISSIONS_REQUEST_FILE =4321;
+
+    String mCurrentPhotoPath;
+    TextView flagLoadImage;
+    ImageView prueba;
+    Bitmap bitmap;
+
     TextInputLayout textInputplaca;
     TextInputLayout textInputmodelo;
     TextInputLayout textInputmarca;
@@ -65,6 +113,7 @@ public class tus_vehiculos extends Fragment {
     EditText txtMarca;
     EditText txtColor;
     EditText txtNasientos;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -140,6 +189,8 @@ public class tus_vehiculos extends Fragment {
     }
 
 
+
+
     private class EliminarVehiculo extends   AsyncTask<URL, Void, String> {
         @Override
         protected String doInBackground(URL... urls){
@@ -185,7 +236,6 @@ public class tus_vehiculos extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
 
     }
 
@@ -254,40 +304,218 @@ public class tus_vehiculos extends Fragment {
             } catch (JSONException e) {
                 Log.e(TAG, "Problem parsing the earthquake JSON results", e);
             }
+        }
+    }
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    /*private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        getActivity().sendBroadcast(mediaScanIntent);
+    }*/
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK)
+            return;
+         bitmap= null;
+        String path = "";
+        if (requestCode == PICK_FROM_FILE){
+            photoURI =data.getData();
+            try {
+               // path = getPathFromURI(photoURI);
+                InputStream is = getActivity().getContentResolver().openInputStream(photoURI);
+                bitmap = BitmapFactory.decodeStream(is);
+                is.close();
+
+
+                int srcWidth = bitmap.getWidth();
+                int srcHeight = bitmap.getHeight();
+                int dstWidth = (int)(srcWidth*0.8f);
+                int dstHeight = (int)(srcHeight*0.8f);
+                Bitmap dstBitmap = Bitmap.createScaledBitmap(bitmap, dstWidth, dstHeight, true);
+                prueba.setImageBitmap(dstBitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else if (requestCode == REQUEST_CAMERA){
+            try{
+                InputStream is = getActivity().getContentResolver().openInputStream(photoURI);
+                bitmap = BitmapFactory.decodeStream(is);
+                is.close();
+                int srcWidth = bitmap.getWidth();
+                int srcHeight = bitmap.getHeight();
+                int dstWidth = (int)(srcWidth*0.8f);
+                int dstHeight = (int)(srcHeight*0.8f);
+                Bitmap dstBitmap = Bitmap.createScaledBitmap(bitmap, dstWidth, dstHeight, true);
+                prueba.setImageBitmap(dstBitmap);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private String bitmapToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        if (bitmap != null){
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream .toByteArray();
+            return Base64.encodeToString(byteArray, Base64.DEFAULT);
+        }else
+            return "";
+    }
+
+    public void launchCam(){
+
+        Intent intent  = new Intent  (MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getActivity().getPackageManager())!=null) {
+
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+                if (photoFile != null) {
+                    photoURI = FileProvider.getUriForFile(getActivity(), "com.example.jose.carpool.fileprovider"
+                            , photoFile);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+
+                }
+            } catch (Exception ex) {
+
+                ex.printStackTrace();
+            }
         }
 
     }
 
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA : {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
+                        launchCam();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }case MY_PERMISSIONS_REQUEST_FILE : {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {//nada
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Menu 1");
-
-
-
         ImageView addCar = (ImageView) getView().findViewById(R.id.btnAgregarVehiculo);
         addCar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-
-                    final Dialog dialog = new Dialog(getActivity());
-                    dialog.setContentView(R.layout.dialog_addcar);
-                    dialog.setTitle("Perfil");
-                    dialog.show();
-
-
+                final Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.dialog_addcar);
+                dialog.setTitle("Perfil");
+                dialog.show();
                 final AppCompatButton btnAdd= (AppCompatButton)dialog.findViewById(R.id.btn_AddCar);
+                final ImageButton btnAddPhoto = (ImageButton)dialog.findViewById(R.id.btn_addPhoto);
+                /*Carga la imagen para mostrartla es una prueba*/
+                prueba = dialog.findViewById(R.id.imageView2);
+                /*To pick car image*/
+                final String [] items = { "Cámara","Galería"};
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>( getActivity()  , android.R.layout.select_dialog_item,items);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Selecciona una imagen");
+                builder.setAdapter(adapter, new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (i == 0){
+
+                            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
+                                    != PackageManager.PERMISSION_GRANTED) {
+
+                                requestPermissions(
+                                        new String[]{Manifest.permission.CAMERA},
+                                        MY_PERMISSIONS_REQUEST_CAMERA);
+                            }else {
+                                launchCam();
+                            }
+                        }
+                        else{
+
+                            Intent intent  = new Intent();
+                            intent.setType("image/*");
+                            intent.setAction(Intent.ACTION_GET_CONTENT);
+                            startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
+                        }
+                    }
+                });
+
+                /*Build options dialog*/
+                final AlertDialog dialogPhoto = builder.create();
+                btnAddPhoto.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick (View v){
+                        dialogPhoto.show();
+                    }
+
+                });
+
                 btnAdd.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //textinputNombre = (TextInputLayout) getView().findViewById(R.id.textNombre);
-                        //txtNombre = textinputNombre.getEditText();
 
                         textInputplaca = dialog.findViewById(R.id.txtInputPlaca);
                         textInputmodelo = dialog.findViewById(R.id.txtInputModelo);
@@ -300,12 +528,9 @@ public class tus_vehiculos extends Fragment {
                         txtMarca = textInputmarca.getEditText();
                         txtColor = textInputcolor.getEditText();
                         txtNasientos = textInputnasientos.getEditText();
-
                         new Thread(new Runnable() {
                             @Override
                             public void run() {
-
-
                                 JSONObject jsonObject = new JSONObject();
                                 try {
                                     jsonObject.put("idUsuario", Integer.parseInt(MainActivity.user.getID()));
@@ -314,12 +539,12 @@ public class tus_vehiculos extends Fragment {
                                     jsonObject.put("marca", txtMarca.getText());
                                     jsonObject.put("color", txtColor.getText());
                                     jsonObject.put("nasientos", Integer.parseInt(txtNasientos.getText().toString()));
-                                    jsonObject.put("img", "0");
+                                    jsonObject.put("img", bitmapToBase64(bitmap));
+                                    System.err.println(jsonObject);
                                 } catch (JSONException e) {
                                     // TODO Auto-generated catch block
                                     e.printStackTrace();
                                 }
-
                                 String BaseURL = "http://200.16.7.170/api/vehiculos/agregar_vehiculo";//solo se agrega "correo/pass"
                                 OutputStream os = null;
                                 InputStream is = null;
@@ -362,7 +587,6 @@ public class tus_vehiculos extends Fragment {
                                     } catch (Exception e) {
                                         e.printStackTrace();
                                     }
-
                                     conn.disconnect();
                                 }
                             }
@@ -373,6 +597,7 @@ public class tus_vehiculos extends Fragment {
             }
             });
     }
+
 
 
 }
