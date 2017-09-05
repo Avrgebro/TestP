20c9de11f7bc8d117b68a823128c0ab54701ce8e
Martín Vásquez <pedromvasquezb@gmail.com>
2017-09-04 21:51:42 -0500

    Add temp edit profile fab

--- app/src/main/java/com/example/jose/carpool/ProfileFragment.java
+++ app/src/main/java/com/example/jose/carpool/ProfileFragment.java
@@ -3,11 +3,13 @@ package com.example.jose.carpool;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileFragment extends Fragment {
     private static String TAG = ProfileFragment.class.getSimpleName();
@@ -21,6 +23,13 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         View mainView = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        FloatingActionButton fabEditProfile = mainView.findViewById(R.id.fab_edit_profile);
+        fabEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Hola", Toast.LENGTH_SHORT).show();
+            }
+        });
         return mainView;
     }
 }
