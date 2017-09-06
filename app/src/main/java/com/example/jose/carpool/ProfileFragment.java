01926b4aa883f963b8d77c08e75237149e4084bb
Martín Vásquez <pedromvasquezb@gmail.com>
2017-09-06 16:43:05 -0500

    Add partial profile picture processing

--- app/src/main/java/com/example/jose/carpool/ProfileFragment.java
+++ app/src/main/java/com/example/jose/carpool/ProfileFragment.java
@@ -1,18 +1,32 @@
 package com.example.jose.carpool;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
+import android.util.Base64;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,7 +36,12 @@ import com.google.gson.Gson;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -34,9 +53,14 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+import static android.app.Activity.RESULT_OK;
 import static android.content.Context.MODE_PRIVATE;
 
 public class ProfileFragment extends Fragment {
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 51242;
+    private static final int REQUEST_PICK_FROM_FILE = 51231;
+    private static final int REQUEST_CAMERA = 52322;
+    private static final int MY_PERMISSIONS_REQUEST_FILE = 12312;
     private static String TAG = ProfileFragment.class.getSimpleName();
     private static String USER_UPDATE_URL = "http://200.16.7.170/api/users/actualizar_perfil";
 
@@ -64,6 +88,10 @@ public class ProfileFragment extends Fragment {
     private boolean editEnabled = false;
     private User mUser;
     private OkHttpClient mHttpClient;
+    private Dialog mPhotoDialog;
+    private String mCurrentPhotoPath;
+    private Uri mCurrentPhotoUri;
+    private Bitmap mProfileBitmap;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -81,19 +109,188 @@ public class ProfileFragment extends Fragment {
                 toggleProfileEdit();
             }
         });
-
         fabSaveProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 updateProfile();
             }
         });
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPhotoSelectionDialog();
+            }
+        });
+        mPhotoDialog = buildPhotoSelectionDialog();
 
         bindUser();
-
         mHttpClient = new OkHttpClient();
 
         return mainView;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "" + requestCode + " " + resultCode + " " + data.getDataString());
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        switch (requestCode) {
+            case REQUEST_PICK_FROM_FILE: {
+                mCurrentPhotoUri = data.getData();
+                setProfileImg(mCurrentPhotoUri);
+                break;
+            }
+            case REQUEST_CAMERA: {
+                setProfileImg(mCurrentPhotoUri);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String permissions[],
+            @NonNull int[] grantResults
+    ) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA : {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    launchCamera();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }case MY_PERMISSIONS_REQUEST_FILE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                }
+                break;
+            }
+        }
+    }
+
+    private void setProfileImg(Uri photoUri) {
+        try {
+            // path = getPathFromURI(mCurrentPhotoUri);
+            InputStream is = getActivity().getContentResolver().openInputStream(photoUri);
+            mProfileBitmap = BitmapFactory.decodeStream(is);
+            is.close();
+
+            int srcWidth = mProfileBitmap.getWidth();
+            int srcHeight = mProfileBitmap.getHeight();
+            int dstWidth = (int)(srcWidth*0.8f);
+            int dstHeight = (int)(srcHeight*0.8f);
+
+            Bitmap dstBitmap = Bitmap.createScaledBitmap(mProfileBitmap, dstWidth, dstHeight, true);
+            profileImg.setImageBitmap(dstBitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String bitmapToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        if (bitmap == null) {
+            return "";
+        }
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    public void launchCamera() {
+        Intent intent  = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            File photoFile;
+            try {
+                photoFile = createImageFile();
+                if (photoFile != null) {
+                    mCurrentPhotoUri = FileProvider.getUriForFile(
+                            getActivity(),
+                            "com.example.jose.carpool.fileprovider",
+                            photoFile
+                    );
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, mCurrentPhotoUri);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private Dialog buildPhotoSelectionDialog() {
+        final String [] items = { "Cámara","Galería" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                getActivity(),
+                android.R.layout.select_dialog_item,
+                items
+        );
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Selecciona una imagen");
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int index) {
+                if (index == 0) {
+                    if (ActivityCompat.checkSelfPermission(
+                            getActivity(),
+                            android.Manifest.permission.CAMERA
+                    ) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(
+                                new String[]{android.Manifest.permission.CAMERA},
+                                MY_PERMISSIONS_REQUEST_CAMERA
+                        );
+                    } else {
+                        launchCamera();
+                    }
+                } else {
+                    Intent intent  = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Complete action using"),
+                            REQUEST_PICK_FROM_FILE
+                    );
+                }
+            }
+        });
+
+        AlertDialog dialogPhoto = builder.create();
+        return dialogPhoto;
+    }
+
+    private void showPhotoSelectionDialog() {
+        if (mPhotoDialog != null) {
+            mPhotoDialog.show();
+        }
     }
 
     private void updateProfile() {
@@ -106,7 +303,15 @@ public class ProfileFragment extends Fragment {
         mUser.setNombre(firstNameInput.getText().toString());
         mUser.setApellido(lastNameInput.getText().toString());
 
-        updateUser();
+        // Run in background
+        AsyncTask<Void, Void, Void> updateUser = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                updateUser();
+                return null;
+            }
+        };
+        updateUser.execute();
     }
 
     private void updateUser() {
@@ -180,13 +385,13 @@ public class ProfileFragment extends Fragment {
     // Shouldn't be here, user serialization should be standard
     // And probably centralized in User class
     private String serializeUser(User user) {
-        JSONObject jsonObject= new JSONObject();
+        JSONObject jsonObject = new JSONObject();
         try {
             jsonObject.put("idUsuario", user.getID());
             jsonObject.put("nombre", user.getNombre());
             jsonObject.put("apellido", user.getApellido());
             jsonObject.put("telefono", user.getTelefono());
-            jsonObject.put("img", "");
+            jsonObject.put("img", bitmapToBase64(mProfileBitmap));
         } catch (JSONException e) {
             Log.e(TAG, "Problem creating JsonObject", e);
             e.printStackTrace();
